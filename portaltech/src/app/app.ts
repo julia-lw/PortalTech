574af25923cf32bb0e1cbb92cd27195e583ac9d5
Júlia L. W. <julia_wecolovis@estudante.sesisenai.org.br>
2026-03-31 16:09:13 -0300

    a

--- portaltech/src/app/app.ts
+++ portaltech/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 import { Home } from './pages/home/home';
 import { Tutoriais } from './pages/tutoriais/tutoriais';
 import { Documentacao } from './pages/documentacao/documentacao';
@@ -7,7 +8,7 @@ import { Autor } from './pages/autor/autor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Home, Tutoriais, Documentacao, Autor],
+  imports: [RouterOutlet, RouterLink, Navbar, Home, Tutoriais, Documentacao, Autor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
