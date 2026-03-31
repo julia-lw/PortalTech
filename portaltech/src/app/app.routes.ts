574d3af6b77611dd446fb331c81e61dcb64e6c4d
Júlia L. W. <julia_wecolovis@estudante.sesisenai.org.br>
2026-03-31 16:57:50 -0300

    a

--- portaltech/src/app/app.routes.ts
+++ portaltech/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
 import { Tutoriais } from './pages/tutoriais/tutoriais';
 import { Documentacao } from './pages/documentacao/documentacao';
 import { Autor } from './pages/autor/autor';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {path: '', component: Home},
     {path: 'tutoriais', component: Tutoriais},
     {path: 'documentacao', component: Documentacao},
-    {path: 'autor', component: Autor}
+    {path: 'autor', component: Autor},
+    {path: '', component: Home},
 ];
